60eada6df1f38e00bcae824c2cc88805b0583e68
Beyza Sönmez <beysonme@k1m43s04.42istanbul.com.tr>
2025-07-15 14:52:17 +0300

    main

--- main.c
+++ main.c
@@ -3,17 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kikiz <kikiz@student.42istanbul.com.tr>    +#+  +:+       +#+        */
+/*   By: beysonme <beyzasonmez565@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 16:06:21 by kikiz             #+#    #+#             */
-/*   Updated: 2025/07/15 14:28:19 by kikiz            ###   ########.fr       */
+/*   Updated: 2025/07/15 14:51:32 by beysonme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#define PROMPT "\001\033[38;5;205m\002minimini1shell$ \001\033[0m\002"
 #include <stdio.h>
 #include <termios.h>
 #include <unistd.h>
 
 int main() {
-
+char *input;
+    while (1)
+	{
+		input = readline(PROMPT);
+		if (!input)
+			break;
+		add_history(input);
+		free(input);
+	}
+	return (0);
 }
+
